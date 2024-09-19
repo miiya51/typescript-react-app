2dacb773abe99314de3a493e7f3d5625795354c6
miiya51 <fw551cp1@gmail.com>
2024-09-19 11:27:20 +0000

    feat: ランダム取得処理追加

--- poke-api/app/page.tsx
+++ poke-api/app/page.tsx
@@ -1,32 +1,52 @@
 "use client";
 import { useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
 
-
-const pokemonImages: string[] = [
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png",
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/445.png",
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/887.png",
-];
-
-const randomPokemonImage = (): string =>{
-  const index = Math.floor(Math.random() * pokemonImages.length);
-  return pokemonImages[index];
+const fetchPokemon = async () =>{
+  const index = Math.floor(Math.random() * 905 + 1);
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + index);
+  const result = await res.json();
+  return result;
 };
+fetchPokemon().then((pokemon) => {
+  console.log(`図鑑番号: ${pokemon['id']}`);
+  console.log(`名前: ${pokemon['name']}`);
+  console.log(`画像URL: ${pokemon['sprites']['front_default']}`);
+});
 
-const IndexPage = () => {
+interface IndexPageProps{
+  id: number;
+  name: string;
+  front_image: string;
+}
+
+const IndexPage : NextPage<IndexPageProps> = (props: IndexPageProps) => {
+  const [pokemonID,setPokemonID] = useState(
+    props.id
+  );  
+  const [pokemonName,setPokemonName] = useState(
+    props.name
+  );
   const [pokemonImageUrl, setPokemonImageUrl] = useState(
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/887.png"
+    props.front_image
   );
 
-  const handleClick = () => {
-    setPokemonImageUrl(randomPokemonImage);
+  const handleClick = async () => {
+    const pokemon = await fetchPokemon();
+    setPokemonID(pokemon['id']);
+    setPokemonName(pokemon['name']);
+    setPokemonImageUrl(pokemon['sprites']['front_default']);
   };
 
   return (
     <div>
       <button onClick={handleClick}>チェンジ</button>
-      <img src={pokemonImageUrl}/>  
-    </div>)
+      <div>
+        <img src={pokemonImageUrl}/>  
+        <p>{pokemonID} {pokemonName}</p>
+      </div>
+    </div>
+  );
 };
 
 export default IndexPage;
